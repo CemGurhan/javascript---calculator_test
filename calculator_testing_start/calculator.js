87ce81a98752a506a98e393b031d19932fd828c0
Cem Gurhan <cemg@live.co.uk>
2022-05-31 21:49:29 +0100

    tried to DRY code. Finished tests

--- calculator_testing_start/calculator.js
+++ calculator_testing_start/calculator.js
@@ -1,14 +1,16 @@
+
 const sum = function(a, b){
     return a + b;
 };
 
-const subtract = (a,b) => a-b ;
+const subtract = (a,b) => sum(a,-b);
 
 const multiply = (a,b) => a*b;
 
 const divide = (a,b) => a/b;
 
 const modulus = (a,b) => a%b;
+
 
 const even = a => {
     if(a%2==0){
@@ -18,7 +20,7 @@ const even = a => {
 };
 
 const odd = a => {
-    if(a%2 == 1){
+    if(!even(a)){
         return true;
     }
     return false;
--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -40,24 +40,151 @@ describe('sum', () => {
 
 describe('subtract', () => {
 
+  test("can subtract from zero",() => {
+
+    expect(subtract(0,5)).toBe(-5);
+
+  });
+
+  test("can subtract two positive numbers",() => {
+
+    expect(subtract(10,5)).toBe(5);
+
+  });
+
+  test("can subtract from a large positive number",() => {
+
+    expect(subtract(Number.MAX_VALUE,5)).toBe(Number.MAX_VALUE - 5);
+
+  });
+
+  test("can subtract two large positive numbers from each other",() => {
+
+    expect(subtract(10_000_000,5_000_000)).toBe(5_000_000);
+
+  });
+
+  test("can subtract two negative numbers",()=>{
+     
+    expect(subtract(-10,-32)).toBe(22);
+
+  });
+
 });
 
 describe('multiply', () => {
+
+  test('can multiply two positive numbers',()=>{
+
+    expect(multiply(3,4)).toBe(12);
+
+  });
+
+  test('can multiply two negative numbers',()=>{
+
+    expect(multiply(-3,-29)).toBe(87);
+
+  });
+
+  test('can multiply a negative number with positive',()=>{
+
+    expect(multiply(-2,237)).toBe(-474);
+
+  })
+
+  test('can multiply decimal numbers',()=>{
+
+    expect(multiply(0.5,0.25)).toBe(0.125);
+
+  })
 
 });
 
 describe('divide', () => {
 
+  test('can divide two positive numbers',()=>{
+
+    expect(divide(15,5)).toBe(3);
+
+  });
+
+  test('can divide two negative numbers',()=>{
+
+expect(divide(-3,-1)).toBe(3);
+
+  });
+
+  test('can divide two large numbers',()=>{
+
+expect(divide(100_000_000,200_000_000)).toBe(0.5);
+
+  })
+
 });
 
 describe('modulus', () => {
+
+  test('can modulo 2 positive numbers',()=>{
+
+    expect(modulus(7,3)).toBe(1);
+
+  });
+
+  test('can modulo a number with 0',()=>{
+
+    expect(modulus(0,2)).toBe(0);
+  })
 
 });
 
 describe('even', () => {
 
+  test('check if a positive number is even',()=>{
+    
+    expect(even(32)).toBe(true);
+
+  });
+
+  test('check if a negative number is even',()=>{
+    
+    expect(even(-12)).toBe(true);
+
+  })
+
+  test('odd number should return false',()=>{
+    expect(even(33)).toBe(false);
+
+  })
+
+  test('zero',()=>{
+    expect(even(0)).toBe(true);
+
+  })
+
 });
 
 describe('odd', () => {
+
+  test('check if a positive number is odd',()=>{
+    
+    expect(odd(75)).toBe(true);
+
+  });
+
+  test('check if a negative number is odd',()=>{
+    
+    expect(odd(-57)).toBe(true);
+
+  })
+
+  test('even number should return false',()=>{
+    expect(odd(176)).toBe(false);
+
+  })
+
+  test('zero',()=>{
+    expect(odd(0)).toBe(false);
+
+  })
 
 });
